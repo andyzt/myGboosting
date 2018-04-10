000e3f7876a2ba4708a1364901df9fc2e304419f
Andrey <andy_evl@mail.ru>
2018-04-10 23:28:01 +0300

    train changed to fit,  readme file modified

--- src/main.cpp
+++ src/main.cpp
@@ -12,20 +12,20 @@ int main(int argc, char** argv) {
 
     args::ArgumentParser parser("mini Gradient Boosting utility");
     args::Group commands(parser, "commands");
-    args::Command train(commands, "train", "Builds model based on provided learning dataset and writes it in output file");
+    args::Command fit(commands, "fit", "Builds model based on provided learning dataset and writes it in output file");
     args::Command predict(commands, "predict", "makes predictions with provided model and test dataset, writes it in output file");
     args::Group arguments(parser, "arguments", args::Group::Validators::DontCare, args::Options::Global);
     args::Positional<std::string> input_file(arguments, "input file", "input for train/predict");
     args::ValueFlag<std::string> column_names_file(arguments, "path", "file containing dataset column names",  { "column_names" });
     args::ValueFlag<std::string> output_file(arguments, "path", "output for train/predict", { "output" });
-    args::ValueFlag<std::string> model_file(arguments, "path", "model for predict", { "model" });
+    args::ValueFlag<std::string> model_file(arguments, "path", "model for predict", { "model-path" });
     args::ValueFlag<std::string> target_column(arguments, "column name", "target column name", { "target" });
     args::HelpFlag h(arguments, "help", "help", { 'h', "help" });
     //args::PositionalList<std::string> pathsList(arguments, "paths", "files to commit");
 
     try {
         parser.ParseCLI(argc, argv);
-        if (train) {
+        if (fit) {
             std::cout << "Train" << std::endl;
 
             auto path = args::get(input_file);
