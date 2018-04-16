1e06ee212ea7d63d49bbcf43b5a8c2bfb855cb62
Eduard Vlasenko <ehduardvlasenko@yandex.ru>
2018-04-17 01:38:05 +0300

    Bugfix in get_dataset.sh

--- testing/datasets/CreditCardFraud/get_dataset.sh
+++ testing/datasets/CreditCardFraud/get_dataset.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # please run "pip install kaggle" to get requirements
 kaggle datasets download -d mlg-ulb/creditcardfraud -w
-head -n 1 creditcardfraud.csv   > column_names.csv
-tail -n +2 creditcardfraud.csv > data.csv
-rm creditcardfraud.csv
+head -n 1 creditcard.csv   > column_names.csv
+tail -n +2 creditcard.csv > data.csv
+rm creditcard.csv
 
